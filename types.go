2522c6d200b9ebd790af18799c5b63b82fd70745
Alejandro Mery <amery@apptly.co>
2024-10-19 21:10:39 +0000

    chore: improve comments
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- types.go
+++ types.go
@@ -4,8 +4,8 @@ import "darvaza.org/slog"
 
 // A Store allows us to create or access Cache namespaces
 type Store interface {
-	// GetCache returns the named cache previously created with
-	// NewCache, or nil if there's no such namespace.
+	// GetCache returns the named cache previously created with NewCache,
+	// or nil if there's no such namespace.
 	GetCache(name string) Cache
 	// NewCache creates a new Cache namespace
 	NewCache(name string, cacheBytes int64, getter Getter) Cache
@@ -16,7 +16,7 @@ type Store interface {
 	SetLogger(log slog.Logger)
 }
 
-// Stats provides an snapshot on the state of a Cache namespace
+// Stats provides a snapshot on the state of a Cache namespace
 type Stats struct {
 	Bytes     int64
 	Items     int64
@@ -29,9 +29,9 @@ type Stats struct {
 type Type int
 
 const (
-	// MainCache is the cache for items we own
+	// MainCache represents the cache for items we owner by the application.
 	MainCache Type = iota + 1
-	// HotCache is the cache for items that seem popular
-	// even if we don't necessarily own
+	// HotCache represents the cache for frequently accessed items, even if
+	// not owned by the application.
 	HotCache
 )
