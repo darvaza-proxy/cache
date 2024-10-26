ffd2774b52e92d17697592461fd9d50e94e57730
Alejandro Mery <amery@apptly.co>
2024-10-26 19:23:41 +0000

    errors: introduce ErrInvalidSink
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- errors.go
+++ errors.go
@@ -1,8 +1,13 @@
 package cache
 
-import "darvaza.org/core"
+import (
+	"darvaza.org/core"
+)
 
 var (
+	// ErrInvalidSink tells the [Sink] isn't in a usable state.
+	ErrInvalidSink = core.QuietWrap(core.ErrInvalid, "%s", "invalid sink")
+
 	// ErrNoData indicates to data was provided.
 	ErrNoData = core.Wrap(core.ErrInvalid, "no data")
 )
